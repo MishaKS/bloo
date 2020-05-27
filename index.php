22e22f05c802cfba0736aff3839565677d929915
mishaks <mishaks@inbox.ru>
2020-05-27 14:58:11 +0600

    ok

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+$servername = "localhost";
+$password = "";
+$dbname = "myDB";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("?????? ???????????: " . $conn->connect_error);
+}
+
+
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+id int(11),
+ data_add text,
+  price text,
+reg_date TIMESTAMP
+)";
+if ($conn->query($sql) === TRUE) {
+    echo "??????? MyGuests ??????? ???????";
+} else {
+    echo "?????? ???????? ???????: " . $conn->error;
+}
+
+
+$conn->close();
+?>
+
+
+
