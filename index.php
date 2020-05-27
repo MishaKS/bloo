4fec97c40e93b187cdb061c2aae1fe5cdb2feb6e
mishaks <mishaks@inbox.ru>
2020-05-27 15:59:48 +0600

    ffff

--- index.php
+++ index.php
@@ -7,9 +7,11 @@ $dbname = "myDB";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+
 if ($conn->connect_error) {
     die("?????? ???????????: " . $conn->connect_error);
 }
+
 
 
 $sql = "CREATE TABLE MyGus (
@@ -17,15 +19,23 @@ id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 firstname VARCHAR(30) NOT NULL,
 lastname VARCHAR(30) NOT NULL,
 email VARCHAR(50),
+ALTER TABLE `id` ADD `3` VARCHAR(40) NOT NULLALTER TABLE `d` ADD `pole_name` VARCHAR(40) NOT NULL
+ALTER TABLE `price` ADD `name` VARCHAR(40) NOT NULL
+
+
+
+
 id int(11),
  data_add text,
   price text,
 reg_date TIMESTAMP
 )";
+
 if ($conn->query($sql) === TRUE) {
-    echo "??????? MyGuests ??????? ???????";
+    echo "";
+
 } else {
-    echo "?????? ???????? ???????: " . $conn->error;
+    echo " " . $conn->error;
 }
 
 
